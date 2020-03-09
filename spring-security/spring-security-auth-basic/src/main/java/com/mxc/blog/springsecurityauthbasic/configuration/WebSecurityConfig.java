d943d8b0b35d68a2ce7051f4203d98eded316107
benweizhu <benwei.zhu@exxonmobil.com>
2020-03-09 12:06:46 +0800

    [spring-security-auth-baisc] antMatchers scenario for Spring security basic(only apply auth to specific path)

---
+++ spring-security/spring-security-auth-basic/src/main/java/com/mxc/blog/springsecurityauthbasic/configuration/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.mxc.blog.springsecurityauthbasic.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    UserDetails userDetails = User.builder().username("admin").password(encoder.encode("password")).roles("ADMIN").build();
+    auth.inMemoryAuthentication().withUser(userDetails);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/greeting").hasRole("ADMIN")
+        .and()
+        .formLogin();
+  }
+}
